9f34193cd9616ed39b9168fae770b1feb4020770
rudra <goutam.prasad.2707@gmail.com>
2026-01-26 16:00:16 +0530

    assets: added images

---
+++ src/assets/images.ts
@@ -0,0 +1,25 @@
+import disha_1 from "public/images/disha-1.webp";
+import disha_2 from "public/images/disha-2.webp";
+import disha_3 from "public/images/disha-3.webp";
+import disha_4 from "public/images/disha-4.webp";
+import disha_5 from "public/images/disha-5.webp";
+import disha_6 from "public/images/disha-6.webp";
+import disha_7 from "public/images/disha-7.webp";
+import disha_8 from "public/images/disha-8.webp";
+import disha_9 from "public/images/disha-9.webp";
+import disha_10 from "public/images/disha-10.webp";
+import disha_11 from "public/images/disha-11.webp";
+
+export {
+	disha_1,
+	disha_2,
+	disha_3,
+	disha_4,
+	disha_5,
+	disha_6,
+	disha_7,
+	disha_8,
+	disha_9,
+	disha_10,
+	disha_11,
+};
